12a28815162a5b6e087a35f79f7e56542111f165
emma16 <emma.zakarian.624@my.csun.edu>
2015-03-23 21:20:25 -0700

    added the footer
    I attached “awesome fonts” to the head (for icons) and changed the
    container to “fluid” in case we want some elements to stretch across
    full width of the screen.

--- wp-content/themes/lambent-theme/footer.php
+++ wp-content/themes/lambent-theme/footer.php
@@ -1,7 +1,24 @@
 	<div id="footer">
 		<?php wp_footer(); ?>
-	</div>
-      
-</div><!-- x content -->
+			<div class="row black">
+
+			<div class="col-xs-4 copyright">
+				<p>© 2015 by Ani Simonian and Emma Zakarian</p>
+			</div>
+
+			<div class="col-xs-4 address">
+				<p>2468 Main Street Los Angeles CA 90012</p>
+			</div>
+
+			<div class="col-xs-3 col-xs-offset-1 social">
+				<ul>
+				<li><a href="http://facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="http://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="http://instagram.com/" target="_blank"><i class="fa fa-instagram"></i> </a></li>
+				</ul>
+			</div>
+		</div>
+		</div>
+</div>
 </body>
 </html>
--- wp-content/themes/lambent-theme/header.php
+++ wp-content/themes/lambent-theme/header.php
@@ -4,13 +4,14 @@
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
 	<?php wp_head();?>
 </head>
 <body>
-  <div class="container">
+  <div class="container-fluid">
 
 		<div class="headerLogo col-xs-12">
 			<a href="http://localhost:8888/lambent/lambent/wp-content/uploads/2015/03/lambent_home_logo1.png">
