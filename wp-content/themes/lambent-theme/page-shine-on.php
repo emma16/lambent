f748e178fba2efcf04e809b3972b70427a54f32b
Ani Simonian <idonttgetit@yahoo.com>
2015-03-26 23:05:53 -0700

    adding custom fields

--- wp-content/themes/lambent-theme/page-shine-on.php
+++ wp-content/themes/lambent-theme/page-shine-on.php
@@ -9,12 +9,25 @@ $shineon = new WP_Query(['post_type'=>'shine-on']);
 
 if ($shineon->have_posts()) : while ($shineon->have_posts()) : $shineon->the_post(); ?>
 
+<?php
+$date = DateTime::createFromFormat('Ymd', get_field('date_of_event'));
 
+?>
 	  	<div class="row">
 	  		<div class="col-xs-12">
 	 			<h1><?php the_title(); ?></h1>
 	 		</div>
 	  	</div>
+
+	  	<div class="col-xs-12 eventDetails">
+ 			<div class="row">
+				Location: <?php the_field('location'); ?>
+			</div>
+
+			<div class="row">
+				Date of Event: <?php echo $date->format('m-d-Y'); ?>
+			</div>
+	 	</div>
 	 	
 		<div class="row">
 			<div class="col-xs-12">
