0387288280b91e4fbe960e277468dfab0f7fdcea
emma16 <emma.zakarian.624@my.csun.edu>
2015-03-26 20:16:02 -0700

    worked on styles, about page

--- wp-content/themes/lambent-theme/header.php
+++ wp-content/themes/lambent-theme/header.php
@@ -4,6 +4,7 @@
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400|Slabo+13px' rel='stylesheet' type='text/css'>
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
@@ -22,7 +23,6 @@
 		</div>
 
 		<div class="navigationBar col-xs-12">
-
 			<ul class="navigationList">
 				<li><a href="<?php bloginfo('url'); ?>/index">Home</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/about">About</a></li>
