2ef39a076af11a32323394c05e724b61f4e85929
Ani Simonian <idonttgetit@yahoo.com>
2015-03-26 22:54:54 -0700

    adding custom fields in code

--- wp-content/themes/lambent-theme/page-shine-on.php
+++ wp-content/themes/lambent-theme/page-shine-on.php
@@ -10,11 +10,21 @@ $shineon = new WP_Query(['post_type'=>'shine-on']);
 if ($shineon->have_posts()) : while ($shineon->have_posts()) : $shineon->the_post(); ?>
 
 
-	  	<h1><?php the_title(); ?></h1>
-	 	<?php the_content(); ?>
-
-
-
+	  	<div class="row">
+	  		<div class="col-xs-12">
+	 			<h1><?php the_title(); ?></h1>
+	 		</div>
+	  	</div>
+	 	
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="<?php the_field('image'); ?>" class="postImage" />
+				<div class="postContent">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	
 	  <?php endwhile; else: ?>
 	    <p>error, not found</p>
 	<?php endif; ?>
