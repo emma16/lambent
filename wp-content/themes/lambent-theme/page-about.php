375d62b39bac92ed5a779af892496635380b1677
Ani Simonian <idonttgetit@yahoo.com>
2015-03-27 01:02:47 -0700

    fixing margins

--- wp-content/themes/lambent-theme/page-about.php
+++ wp-content/themes/lambent-theme/page-about.php
@@ -10,7 +10,7 @@ Template Name: About Page
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	  	<h1><?php the_title(); ?></h1>
+	  	<h1 class="aboutPageTitle"><?php the_title(); ?></h1>
 	 	<?php the_content(); ?>
 
 
